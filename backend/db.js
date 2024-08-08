25c7553d3532158d0779bc9cf255b68f42a734cc
DarshanCHMSR <chdarshan99@gmail.com>
2024-08-09 00:05:34 +0530

    creation and updation of modules

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const mongoURI ="mongodb://localhost:27017/";
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,()=>{
-
-    })
-}
- module.exports= connectToMongo();
+    mongoose.connect(mongoURI,{
+    
+    },).then(()=>console.log("connected successfully"))
+    .catch((err)=>{console.log(err)})
+};
+ module.exports= connectToMongo;
