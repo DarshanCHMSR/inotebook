d9087bfd14876d680f1cbc6655702715ddc93aa9
DarshanCHMSR <chdarshan99@gmail.com>
2024-08-09 23:16:39 +0530

    server as chaged from 3000 to 5000 and authetication was created in thunderclient to get new request

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,4 +20,6 @@ const UserSchema =new Schema({
     },
     
 });
-module.exports=mongoose.model('user',UserSchema);   
+const User = mongoose.model('user',UserSchema); 
+
+module.exports= User ;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const User = require('../models/User');
-const router=express.Router()
-//create a user using : POST "/api/auth/". Doesn't require Auth
-router.post('/',(req,res)=>{
-   console.log(req.body);
-   const user = User(req.body);
-   user.save();
-    res.send(req.body);
+const router=express.Router();
+const { body, validationResult } = require('express-validator');
+
+//create a user using : POST "/api/auth/createuser". Doesn't require Auth or no login required 
+router.post('/creareuser',[
+    body('name','enter a valid name').isLength({ min: 3}),
+    body('email','enter a valid email').isEmail(),
+    body('password','password must be atleast of 5 chacters').isLength({ min: 5 }),
+],async(req,res)=>{
+  // if there are error returns bad requests and the errors
+
+try {
+  
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // CHECKS WHETHER THE USER WITH THIS EMAIL EXISTS ALREADY
+    let user=await User.findOne({email:req.body.email});
+    if(user){
+      return res.status(400).json({error:"sorry a user with this email already exists "});
+    }
+    //create a new user
+   user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+    //   .then(user => res.json(user))
+    //   .catch(err=>console.log(err),
+    // res.json({error:'please enter a unique email address',message:err.message}))
+    res.json({user})
+    // catches the error 
+    }catch (error) {
+  console.log(error.message);
+  res.status(500).send("some error as occured")
+    }
+    
 })
 module.exports=router
