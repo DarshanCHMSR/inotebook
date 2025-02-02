c4c51a0702c3708bc2c0729c763782e675983d29
DarshanCHMSR <chdarshan99@gmail.com>
2025-02-03 00:59:02 +0530

    updated one

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,7 +9,7 @@ function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   }
   return (
-    <div style={{height:'50px'}}>
+    <div >
     {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
   <strong>{captilize(props.alert.type)}</strong>:{props.alert.msg}
 </div>}
